24520092594107bde525fea7e424f467aaee315e
Dmitry <dimk00z@gmail.com>
2020-09-27 12:51:06 +0300

    flask_admin

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+
 from flask import Flask
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
+
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+from flask_admin.contrib.sqla import ModelView
+
+from flask_admin import Admin
+
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
@@ -12,3 +18,8 @@ migrate = Migrate(app=app, db=db)
 
 manager = Manager(app=app)
 manager.add_command('db', MigrateCommand)
+
+from models import *
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
--- config.py
+++ config.py
@@ -3,3 +3,4 @@ class Configuration(object):
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://test1:Test1P@ssw0rd@localhost/test1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SERVER_NAME = "172.24.225.165:5000"
+    SECRET_KEY = "secret"
